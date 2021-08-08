9f29c03527d99b5cb2758db29c25d747bc6180b0
fahadaleem <faleem396@gmail.com>
2021-08-08 16:43:45 +0500

    Navbar Created

---
+++ js/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        logo:"./assets/swing-logo.png",
+        applyBtn:`<li class="nav-item link-btn">
+        <a class="nav-link" href="#">Apply Now</a>
+      </li>`,
+      loginBtn:`<li class="nav-item link-btn">
+      <a class="nav-link" href="#">Login</a>
+    </li>`
+    },
+})
